6fbf388997adf237bdc4a35425ef48308b412b94
DusanMinic <dushanminic@gmail.com>
2016-03-01 21:00:47 +0100

    Add DELETE todos route

--- server.js
+++ server.js
@@ -54,6 +54,23 @@ todos.push(body);
 	res.json(body);
 });
 
+//DELETE /todos/:id
+
+app.delete('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+
+		if(!matchedTodo) {
+			res.status(404).json({"error": "no todo found with that id"});
+		} else {
+			todos = _.without(todos, matchedTodo);
+			res.json(matchedTodo);
+		}
+	
+
+});
+
+// EXPRESS server
 
 app.listen(PORT, function () {
 	console.log('Express listening on port: ' + PORT);
