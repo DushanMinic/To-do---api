ca0a732a3ac1c0a76f6546953cde686e1f82f6c0
DusanMinic <dushanminic@gmail.com>
2016-03-02 18:47:47 +0100

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Dusan',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Dusan', 
+// 	// 	age: 26
+// 	// };
+// 	obj.age = 26;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+
+var grades = [15, 37];
+function gradesAdd (obj) {
+	obj.push(221);
+	debugger;
+
+	// obj = [12, 11, 29];
+}
+
+
+gradesAdd(grades);
+console.log(grades);
